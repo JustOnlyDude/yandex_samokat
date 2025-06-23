89c5ec68cfe949d03a419060693bca524362909b
Dmitriy Tazov <srtbox@yandex.ru>
2025-06-23 16:36:26 +0300

    Third commit

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_BASE = 'https://762aab9e-f7fa-4065-921a-f32909114400.serverhub.praktikum-services.ru'
+URL_BASE = 'https://66b290f6-f730-4be3-a585-4bc9f6ce5614.serverhub.praktikum-services.ru'
 ORDER_CREAT ='/api/v1/orders'
 ORDER_BY_TRACK ='/api/v1/orders/track'
